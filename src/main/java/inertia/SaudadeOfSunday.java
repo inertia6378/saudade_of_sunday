266d073cbc8ad5f96cbb713c499d171c979edd82
Preston Harms <prestonharms5@gmail.com>
2020-04-27 16:18:38 -0700

    04-27-20: Added Maven and angular configurations for auto build and deployment

---
+++ src/main/java/inertia/SaudadeOfSunday.java
@@ -0,0 +1,11 @@
+package inertia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudadeOfSunday {
+    public static void main(String args[]){
+        SpringApplication.run(SaudadeOfSunday.class, args);
+    }
+}
