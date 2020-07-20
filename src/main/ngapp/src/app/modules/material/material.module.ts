5a953de4565a9d197e84147fa49b894dace3d730
Preston <prestonharms5@gmail.com>
2020-07-20 15:50:57 -0700

    07-20-20: Upgraded the angular verion

--- src/main/ngapp/src/app/modules/material/material.module.ts
+++ src/main/ngapp/src/app/modules/material/material.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core';
-import {MatButtonModule, MatCardModule, MatSidenavModule, MatIconModule, MatToolbarModule} from '@angular/material';
 import {MatListModule} from '@angular/material/list';
 import {MatTableModule} from '@angular/material/table';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
